7060dba850cb7f471ea229e271a59cb778343fc0
deepak c. <deepakchoudhary3365@gmail.com>
2025-03-02 21:15:02 +0530

    Time: 3 ms (32.71%) | Memory: 14.8 MB (44.73%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -1,23 +1,57 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        //using two pointers same as merge sorted array
         int m=nums1.size();
         int n=nums2.size();
-        unordered_map<int,int>mpp;//made unordred map
-
-        for(auto &pair:nums1){//checking each pair of nums1;
-            mpp[pair[0]]=mpp[pair[0]]+pair[1];//adding IDs and the value i.e.mpp[1]=mpp[1]+mpp[2]
-        }
-
-        for(auto &pair:nums2){//checking each pair of nums2
-            mpp[pair[0]]=mpp[pair[0]]+pair[1];
-        }
         vector<vector<int>>res;
-        for(auto &it:mpp){
-            res.push_back({it.first,it.second});
+
+        int i=0;
+        int j=0;
+
+        while(i<m&&j<n){
+            if(nums1[i][0]<nums2[j][0]){
+                res.push_back(nums1[i]);//if nums1's pair id is less then push-back the whole pair
+                i++;//increase i by 1
+            }
+            else if(nums1[i][0]>nums2[j][0]){//if nums2's pair id is less then push-back the whole pair
+                res.push_back(nums2[j]);
+                j++;
+            }
+            else{
+                res.push_back({nums1[i][0],nums1[i][1]+nums2[j][1]});//if both ids are same get 1 id from both ids and sum the value of both pair of both aray
+                i++;//increase by 1
+                j++;//increase by 1
+            }
         }
-        sort(res.begin(),res.end());
+        while(i<m){//
+                res.push_back(nums1[i]);
+                i++;
+            }
+        while(j<n){
+            res.push_back(nums2[j]);
+            j++;
+        }
+
         return res;
+
+        // int m=nums1.size();
+        // int n=nums2.size();
+        // unordered_map<int,int>mpp;//made unordred map
+
+        // for(auto &pair:nums1){//checking each pair of nums1;
+        //     mpp[pair[0]]=mpp[pair[0]]+pair[1];//adding IDs and the value i.e.mpp[1]=mpp[1]+mpp[2]
+        // }
+
+        // for(auto &pair:nums2){//checking each pair of nums2
+        //     mpp[pair[0]]=mpp[pair[0]]+pair[1];
+        // }
+        // vector<vector<int>>res;
+        // for(auto &it:mpp){
+        //     res.push_back({it.first,it.second});
+        // }
+        // sort(res.begin(),res.end());
+        // return res;
 
     }
 };
