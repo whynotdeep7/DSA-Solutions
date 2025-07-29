a3a277e7ac6ccf5d47af7bbc44f46d85d9f66b1f
deepak c. <deepakchoudhary3365@gmail.com>
2025-07-29 18:58:46 +0530

    Time: 85 ms (7.23%) | Memory: 85.2 MB (5.77%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        vector<int> grbge;
         int n = nums.size();
-        vector<int> nums_copy = nums;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= 0) {
-                grbge.push_back(nums[i]);
-            }
-        }
-
         unordered_map<int, int> mpp;
         int missing = 0;
         for (int i = 1; i <= n + 1; i++) {
